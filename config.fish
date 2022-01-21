e2fd6aa6407f6ef3be162833c6c61ae197258651
Eko450-eng <ekrem@wipdesign.de>
2022-01-21 21:15:23 +0100

    Quicksave

--- config.fish
+++ config.fish
@@ -5,7 +5,10 @@
 #		exec tmux
 #	end
 export JAVA_HOME=/usr/lib/jvm/default
+#export GEM_HOME=(ruby -e 'puts Gem.user_dir')
+set GEM_HOME = /home/eko/.local/share/gem/ruby/3.0.0
 set PATH = $JAVA_HOME/bin:$PATH
-set PATH = $JAVA_HOME/bin:$PATH:.emacs.d/bin
+set PATH = $JAVA_HOME/bin:$PATH:.emacs.d/bin:/home/eko/.local/share/gem/ruby/3.0.0/bin
+
 
 #export EDITOR=nvim
